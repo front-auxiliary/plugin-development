1a72246a1bbd2630197cca92545b7eeb3f2f8bda
mac <15711367521@163.com>
2019-09-22 19:07:40 +0800

    正则ok

--- node/utils/index.js
+++ node/utils/index.js
@@ -17,29 +17,50 @@ class Parser {
 
     return this;
   }
-  getAttr(attrStr){
+  includegraphicsFormat(attrStr){
     const attrReg = /\[(.*?)\]/g;
+    const srcReg = /\{(.*?)\}/g;
+    let attrObj={};
     attrStr.replace(attrReg,(str)=>{
       let newStr = str,
-          attrs = [],
-          attrObj={};
+          attrs = [];
       newStr= newStr.replace(/\[|\]/g,'');
       attrs = newStr.split(',');
       attrs.map((item)=>{
         let everyAttr = item.split('=');
         attrObj[everyAttr[0]] = everyAttr[1]
       })
-      return {...attrObj};
+      attrStr.replace(srcReg,(str)=>{
+        let newSrc = str;
+        newSrc= newSrc.replace(/\{|\}/g,'');
+        attrObj.src = newSrc;
+      })
+      
     })
-
-
+    return {...attrObj};
   }
+  // \documentclass
+  delhander (str){
+    let docReg = /\\documentclass(\[(.*?)\])*\{(.*?)\}/g
+    let begnReg = /\\begin\{(.*?)\}/g
+    let endReg = /\\end\{(.*?)\}/g
+    let usepackageReg = /\\usepackage(\[(.*?)\])*\{(.*?)\}/g
+    // console.log('----',str)
+    str = str.replace(docReg,(a)=>{
+      console.log(a,"ppp");
+      return a
+    })
+  }
+
   imgFormat(){
     // \includegraphics[width=71.69045005488475mm]{/home/lsk/Desktop/Gaosi/formula_19_0212/data/test111/122/旧/6_0_img/572_77_883_319.jpg}
     const imgReg = /\\includegraphics\[(.*?)\]\{(.*?)\}/g
-   
+    this.delhander(this.stream )
+    return ;
     this.stream = this.stream.replace(imgReg,(str)=>{
-      this.getAttr(str)
+      // console.log( this.includegraphicsFormat(str))
+     
+      // this.getAttr(str)
       return str;
     })
   }
